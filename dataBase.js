9e78e16fc2307f800d7f31584d994b7b0f7adbb3
Netanya Vaz <nv2013@hw.ac.uk>
2025-10-28 23:08:53 +0400

    database connection

--- dataBase.js
+++ dataBase.js
@@ -1,8 +1,8 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const con = mysql.createConnection({
     host: "localhost",
-    user: "yourusername",
-    password: "yourpassword"
+    user: "NodeClient",
+    password: "NodeClient123"
 });
 
 con.connect(function(err) {
