3b20bd806544f39acf49821ba30150349fb2fb82
Netanya Vaz <nv2013@hw.ac.uk>
2025-11-05 23:38:10 +0400

    upd

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -17,7 +18,6 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(__dirname));
 
-const mysql = require('mysql2');
 const con = mysql.createConnection({
     host: "localhost",
     user: "NodeClient",
