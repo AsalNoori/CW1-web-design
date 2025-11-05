1772c521c9c8508483fde3ab7a0fa7978d9db249
NetanyaVaz <netanyavaz1445@gmail.com>
2025-11-05 19:30:45 +0400

    Configure MySQL connection and create Users table
    Added database name and SQL query to create Users table.

--- dataBase.js
+++ dataBase.js
@@ -2,10 +2,16 @@ const mysql = require('mysql2');
 const con = mysql.createConnection({
     host: "localhost",
     user: "NodeClient",
-    password: "NodeClient123"
+    password: "NodeClient123",
+    database: "ShipMe"
 });
 
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
+    var sql = "CREATE TABLE Users (UserID INT AUTO_INCREMENT PRIMARY KEY, FirstName VARCHAR(50), LastName VARCHAR(50), Email VARCHAR(50), PhoneNumber VARCHAR(10), Password VARCHAR(255))";
+    con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+    });
 });
