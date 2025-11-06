a893be99a6a91b703ac4f511ecd7cdb5bf2d2f70
Netanya Vaz <nv2013@hw.ac.uk>
2025-11-06 13:52:13 +0400

    database

--- ShipMe.sql
+++ ShipMe.sql
@@ -1,3 +1,4 @@
+CREATE DATABASE ShipMe;
 USE ShipMe;
 CREATE TABLE Users (UserID INT AUTO_INCREMENT PRIMARY KEY, FirstName VARCHAR(50), LastName VARCHAR(50), Email VARCHAR(100) UNIQUE, PhoneNumber VARCHAR(10) UNIQUE, Password VARCHAR(100));
 INSERT INTO Users (FirstName, LastName, Email, PhoneNumber, Password) VALUES
